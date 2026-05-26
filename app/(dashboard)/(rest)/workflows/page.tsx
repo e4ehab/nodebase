47ae9ee3888fbcb28fdad33e102b0d66e44fca4d
mohamed ehab <e4ehap@gmail.com>
2026-05-26 19:47:42 +0300

    11. Workflows CRUD
    - Update workflow schema
    
    - use prefetch for data prefetching
    
    - Create workflow API
       - create (premium procedure)
       - read
       - update
       - delete
    
    - if the user is not on pro plan when create pop up an upgrade window otherwise create the workflow

--- app/(dashboard)/(rest)/workflows/page.tsx
+++ app/(dashboard)/(rest)/workflows/page.tsx
@@ -1,11 +1,25 @@
 import { requireAuth } from "@/lib/auth.utils";
+import { prefetchWorkflows } from "@/features/workflows/server/prefetch";
+import { HydrateClient } from "@/trpc/server";
+import { ErrorBoundary } from "react-error-boundary";
+import { Suspense } from "react";
+import { WorkflowsList, WorkflowsContainer } from "@/features/workflows/components/workflows";
 
-const Page = async() => {
+const Page = async () => {
     await requireAuth(); // this will throw an error if the user is not authenticated, which will be caught by the error boundary in layout.tsx and redirect the user to the login page
+    prefetchWorkflows(); // prefetch workflows data for faster loading when the user navigates to the workflows page
+
     return (
-        <div>
-            <h1>Workflows</h1>
-        </div>
-    )   
+        <WorkflowsContainer>
+            <HydrateClient>
+                <ErrorBoundary fallback={<p className="text-red-500">Error</p>}>
+                    <Suspense fallback={<p>Loading...</p>}>
+                        <WorkflowsList />
+                    </Suspense>
+                </ErrorBoundary>
+            </HydrateClient>
+        </WorkflowsContainer>
+
+    );
 }
 export default Page;
---
+++ features/workflows/hooks/use-workflows.ts
@@ -0,0 +1,28 @@
+/* Hook to fetch all workflows using suspense */
+import { useMutation, useQueryClient, useSuspenseQuery } from "@tanstack/react-query";
+import { useTRPC } from "@/trpc/client";
+import { toast } from "sonner";
+//-------------------------------------------------------------------------//
+export const useSuspenseWorkflows = () => {
+    // "fetch all my workflows from the server, block until they're ready, and give me back fully typed data."
+    const trpc = useTRPC();
+
+    return useSuspenseQuery(trpc.workflows.getMany.queryOptions());
+}
+//-------------------------------------------------------------------------//
+/* Hook to create a new workflow */
+export const useCreateWorkflow = () => {
+    const queryClient = useQueryClient();
+    const trpc = useTRPC();
+
+    return useMutation(trpc.workflows.create.mutationOptions({
+        onSuccess: (data) => { // get the data of the created workflow
+            toast.success(`workflow "${data.name}" created successfully`); // show success toast with the name of the created workflow
+            queryClient.invalidateQueries(trpc.workflows.getMany.queryOptions(),); // invalidate the getWorkflows query to refetch the list with the new workflow
+        },
+        onError: (error) => { // get the error object thrown
+            toast.error(`Failed to create workflow: ${error.message}`); // show error toast with the error message
+        },
+    })
+    );
+};
