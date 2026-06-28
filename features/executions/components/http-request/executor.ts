1a92a199907d0282a1d66bb6b87934f9247e54da
mohamed ehab <e4ehap@gmail.com>
2026-06-28 22:55:03 +0300

    20. Node templating
    - Refactor "variableName" in executor
    - Implement templating syntax
      - Allow dynamic body
      - Allow dynamic endpoint

--- features/executions/components/http-request/executor.ts
+++ features/executions/components/http-request/executor.ts
@@ -1,11 +1,19 @@
 import type { NodeExecutor } from "@/features/executions/types";
 import { NonRetriableError } from "inngest";
 import ky, { type Options as KyOptions } from "ky";
+import Handlebars from "handlebars";
+
+Handlebars.registerHelper("json", (context) => { // makes the qoute from &qout; to {} so the json result is correct
+    const jsonString = JSON.stringify(context, null, 2);
+    const safeString = new Handlebars.SafeString(jsonString);
+
+    return safeString;
+});
 
 type HttpRequestData = {
-    variableName?: string;
-    endpoint?: string;
-    method?: "GET" | "POST" | "PUT" | "PATCH" | "DELETE";
+    variableName: string;
+    endpoint: string;
+    method: "GET" | "POST" | "PUT" | "PATCH" | "DELETE";
     body?: string;
 };
 
@@ -17,6 +25,7 @@ export const httpRequestExecutor: NodeExecutor<HttpRequestData> = async ({
 }) => {
     // TODO: Publish "loading" state for http request
 
+    // the following if statements are run time validations ensuring the users are passing this 
     if (!data.endpoint) {
         // TODO: Publish "error" state for http request
         throw new NonRetriableError("HTTP Request node: No endpoint configured");
@@ -24,19 +33,34 @@ export const httpRequestExecutor: NodeExecutor<HttpRequestData> = async ({
 
     if (!data.variableName) {
         // TODO: Publish "error" state for http request
-        throw new NonRetriableError("Variable name not configured");
+        throw new NonRetriableError("HTTP Request node: Variable name not configured");
+    }
+
+    if (!data.method) {
+        // TODO: Publish "error" state for http request
+        throw new NonRetriableError("HTTP Request node: method not configured");
     }
 
     const result = await step.run("http-request", async () => {
-        const endpoint = data.endpoint!;
-        const method = data.method || "GET";
+        // http://..../{{todo.httpResponse.data.userId}}
+        const endpoint = Handlebars.compile(data.endpoint)(context);
+        //  handlebars make the http request chainable (data endpoint + context)
+        //  data.endpoint -> is the template the user configured on the node
+        //  contex -> is the data from previous node
+        //  compined they produce the final reql url: template + context filled in the acutual url (https://api.example.com/users/456)
+        const method = data.method;
 
         const options: KyOptions = { method };
 
+
         if (["POST", "PUT", "PATCH"].includes(method)) {
-            options.body = data.body;
-            options.headers = { "Content-Type": "application/json" }; // ensure {POST, Put, Patch} requests doesn't get rejected, because headers are important part of http request
-        }
+            const resolved = Handlebars.compile(data.body || "{}")(context);
+            JSON.parse(resolved);
+            options.body = resolved;
+            options.headers = {
+                "Content-Type": "application/json", // ensure {POST, Put, Patch} requests doesn't get rejected, because headers are important part of http request
+            }
+        };
 
         const response = await ky(endpoint, options);
         const contentType = response.headers.get("content-type");
@@ -52,18 +76,11 @@ export const httpRequestExecutor: NodeExecutor<HttpRequestData> = async ({
             },
         };
 
-        if (data.variableName) {
-            return {
-                ...context,
-                [data.variableName]: responsePayload,
-            }
-        }
-
-        // Fallback to direct httpResponse for backward compatibility
         return {
             ...context,
-            ...responsePayload,
-        };
+            [data.variableName]: responsePayload,
+        }
+
     });
 
     // TODO: Publish "success" state for http request
