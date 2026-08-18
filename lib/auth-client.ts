189a768f6b2da7604d4e379299e271f750186247
mohamed ehab <e4ehap@gmail.com>
2026-08-19 02:05:04 +0300

    31.6
    Fixed  by removing the absolute baseURL.

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -2,7 +2,5 @@ import { createAuthClient } from "better-auth/react"
 import { polarClient } from "@polar-sh/better-auth"
 
 export const authClient = createAuthClient({
-    /** The base URL of the server (optional if you're using the same domain) */
     plugins: [polarClient()],
-    baseURL: process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000",
 })
